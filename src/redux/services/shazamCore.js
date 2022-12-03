d698316586f83eda76a3bb529ef00d8a25200b24
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-12-03 10:19:34 +0100

    getting fetch code from rapid api vs code extension

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,12 @@
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '17802d2f93mshb3a0c25585f3219p1d255cjsn6d073cd2df31',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+  }
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
