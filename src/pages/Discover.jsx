ca8b192f6c4395dcd9648d54f0b499890d19d654
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-12-03 12:28:54 +0100

    added loader element

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -4,8 +4,9 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore'
 
 const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery()
-  console.log(data)
   const genreTitle = 'pop'
+  if (isFetching) return <Loader title="Loading songs..." />
+  if (error) return <Error />
   return (
     <div className="flex flex-col">
       <div
@@ -29,7 +30,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
