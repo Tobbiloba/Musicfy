562d7efbedb70780f5d08a73f10ee7361741a4a4
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-12-04 18:31:40 +0100

    sizing play pause button

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,10 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
 
-export default PlayPause;
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle className="text-gray-300" size={35} onClick={handlePlay} />
+  ) : (
+    <FaPlayCircle className="text-gray-300" size={35} onClick={handlePlay} />
+  )
+
+export default PlayPause
