3f0a070ba651b2bfdeed1258390e06445b4a5208
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-12-04 16:26:09 +0100

    importing necessary files for songCard component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,8 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
-export default SongCard;
+import PlayPause from './PlayPause'
+import { playPause, setActiveSongs } from '../redux/features/playerSlice'
+const SongCard = () => <div>SongCard</div>
+
+export default SongCard
