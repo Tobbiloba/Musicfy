6ba0395d124f5a67e58ab854e69d0666ff7e2eaa
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-12-04 18:32:16 +0100

    fixed error

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -2,7 +2,7 @@ import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
 
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
   isPlaying && activeSong?.title === song.title ? (
-    <FaPauseCircle className="text-gray-300" size={35} onClick={handlePlay} />
+    <FaPauseCircle className="text-gray-300" size={35} onClick={handlePause} />
   ) : (
     <FaPlayCircle className="text-gray-300" size={35} onClick={handlePlay} />
   )
