18cbeca06fc0e3e300a74962caf2cf29d4cc57a6
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-12-08 01:29:10 +0100

    added onclick function

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -34,9 +34,15 @@ const Sidebar = () => {
       {/* mobile view */}
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
-          <RiCloseLine className="w-6 h-6 text-white mr-2" />
+          <RiCloseLine
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(false)}
+          />
         ) : (
-          <HiOutlineMenu className="w-6 h-6 text-white mr-2" />
+          <HiOutlineMenu
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(true)}
+          />
         )}
       </div>
       <div
@@ -45,7 +51,7 @@ const Sidebar = () => {
     ${mobileMenuOpen ? 'left-0' : '-left-full'}`}
       >
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-        <NavLinks />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   )
